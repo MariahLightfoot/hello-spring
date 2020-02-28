81565753726f83d15e03a176ac86172f9cce8cf1
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-02-28 13:52:23 -0600

    practicing using for each loop in template

--- src/main/java/org/LaunchCode/hellospring/controllers/HelloController.java
+++ src/main/java/org/LaunchCode/hellospring/controllers/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -50,5 +53,21 @@ public class HelloController {
         } else {
             return "Hallo, " + name + "!";
         }
+    }
+
+    // using to test iteration template hello-list
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("E");
+        names.add("MC2");
+        names.add("Ram");
+        names.add("Ted");
+
+        // first argument is variable template will see
+        // second is value that variable should have
+        model.addAttribute("names", names);
+
+        return "hello-list";
     }
 }
